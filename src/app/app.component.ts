8f278b6f4266e746c5bafcc0f80c9aab125d949c
José Emiliano <araujo_26@yahoo.com>
2022-08-22 02:29:36 -0300

    Create photos module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+
+  photos: {url: string, description: string}[] = [
+    {
+      url: 'https://media.discordapp.net/attachments/398988559123742723/1010745744908615731/unknown.png',
+      description: 'brabuleta'
+    },
+    {
+      url: 'https://media.discordapp.net/attachments/398988559123742723/1010745744908615731/unknown.png',
+      description: 'brabuleta'
+    }
+  ]
 }
