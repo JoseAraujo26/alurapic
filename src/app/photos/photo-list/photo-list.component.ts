74a6c44056c7120d560d7ac4893a9c6c7fb755c0
José Emiliano <araujo_26@yahoo.com>
2022-08-29 18:05:49 -0300

    Aula 4 - Subject e Debounce (RXJS)

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IPhotos, PhotosService } from '../photos.service';
+// import { debounceTime } from 'rxjs/operators'
+import { Subject, debounceTime } from 'rxjs';
+
+import { IPhotos } from '../photos.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: IPhotos[] = []
   filter: string = ''
+  debounce: Subject<string> = new Subject<string>()
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos']
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter)
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe()
   }
 }
