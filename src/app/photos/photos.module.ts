adb92bb956740d82999b64cff7ec8cdc4e18da84
José Emiliano <araujo_26@yahoo.com>
2022-08-30 16:20:13 -0300

    Aplicação separada em Modulos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -8,21 +8,18 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescriptionPipe } from './photo-list/photos/filter-by-description.pipe';
 import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
 
 
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescriptionPipe,
-    LoadButtonComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule,
-    HttpClientModule
+    PhotoListModule,
+    PhotoFormModule,
+    PhotoModule
   ],
   exports: [ ]
 })
