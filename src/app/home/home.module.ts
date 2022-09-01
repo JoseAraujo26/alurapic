e36a6f110eb90b0e63aa37a642cde6f2e5fbd48c
José Emiliano <araujo_26@yahoo.com>
2022-09-01 02:54:52 -0300

    Autenticação adicionada

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
+
+
+
+@NgModule({
+  declarations: [
+    SigninComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    VmessageModule
+  ]
+})
+export class HomeModule { }
