efdef717d3b34b60bf022d923307132d87213fe6
José Emiliano <araujo_26@yahoo.com>
2022-09-16 11:56:55 -0300

    Configurando rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private titleService: Title
+  ) { }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(map(() => this.activatedRoute))
+      .pipe(map(route => {
+        while (route.firstChild) route = route.firstChild;
+        return route
+      }))
+      .pipe(switchMap(({ data }) => data))
+    .subscribe(({ title }) => this.titleService.setTitle(title))
+  }
+}
