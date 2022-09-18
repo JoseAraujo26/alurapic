6511b77ccec70f4fe56f8eb9fdcbe0461f865103
José Emiliano <araujo_26@yahoo.com>
2022-09-18 00:39:45 -0300

    Exibindo o progresso

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserService } from 'src/app/core/user/user.service';
+import { Component, OnInit } from '@angular/core';
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { finalize } from 'rxjs';
+
 import { PhotoService } from '../photo.service';
+import { UserService } from 'src/app/core/user/user.service';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
 @Component({
   selector: 'app-photo-form',
@@ -14,12 +19,14 @@ export class PhotoFormComponent implements OnInit {
   photoForm!: FormGroup
   file!: File
   preview!: string
+  percentDone = 0
 
   constructor(
     private photosService: PhotoService,
     private formBuilder: FormBuilder,
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private alertService: AlertService
   ) { }
 
   ngOnInit(): void {
@@ -35,7 +42,20 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.controls['allowComments'].value
     this.photosService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['/user', this.userService.getUserName()]))
+      .pipe(finalize(() => {
+        this.router.navigate(['/user', this.userService.getUserName()])
+      }))
+      .subscribe((event: HttpEvent<any>) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.percentDone = Math.round(100 * event.loaded / (event.total ?? 1))
+        } else if (event instanceof HttpResponse) {
+          this.alertService.success('Upload complete', true)
+        }
+      },
+      err => {
+        console.log(err)
+        this.alertService.danger('Upload error')
+      })
   }
 
   handleFile(file: File) {
